3ece1354d7b1205988151b1a382e27b2b17aad49
Pandya Arya Daniswara <pandya.arya@jkt1.ebdesk.com>
2023-10-25 15:09:13 +0700

    feat: send to client

--- src/controller/sse.py
+++ src/controller/sse.py
@@ -1,16 +1,26 @@
+from collections import defaultdict
+
 from fastapi import APIRouter, Request
 from sse_starlette.sse import EventSourceResponse
+
 from src.connection.broker import broker
 
 router = APIRouter()
 
+clients = defaultdict(list)
 @router.get("/notif-upload")
-async def notif_upload_endpoint(request: Request):
+async def notif_upload_endpoint(request: Request, id: str):
     async def event_generator():
         while True:
             if await request.is_disconnected():
                 break
             message = await broker.consume_data()
             if message:
-                yield {"event": "notification", "data": message}
+                if id in clients and len(clients[id]) > 0:
+                    yield {"event": "notification", "data": message}
+    # Tambahkan koneksi klien ke "clients" saat klien terhubung
+    clients[id].append(event_generator())
+    print(clients)
+
+    # Menggunakan EventSourceResponse untuk mengirim data ke klien
     return EventSourceResponse(event_generator())
--- src/service/sse.py
+++ src/service/sse.py
@@ -1,19 +1,24 @@
-import asyncio
+from fastapi import Request
 from sse_starlette.sse import EventSourceResponse
-
+from typing import Callable
 from src.connection.broker import broker  # Import your broker
 
 connected_clients = set()
 
-async def notif_upload():
-    return EventSourceResponse(event_generator())
-
-async def event_generator():
+async def notif_upload(request: Request, filter_func: Callable, user_id: str):
     while True:
-        try:
-            message = await broker.consume_data()
-            if message:
-                yield {"event": "notification", "data": message}
-        except asyncio.CancelledError:
-            print("disconnect")
+        if await request.is_disconnected():
             break
+        message = await broker.consume_data()
+        if message:
+            if filter_func(user_id, message):
+                yield {"event": "notification", "data": message}
+
+
+# async def event_generator():
+#     while True:
+#         if await request.is_disconnected():
+#             break
+#         message = await broker.consume_data()
+#         if message:
+#             yield {"event": "notification", "data": message}
